742a703f24e85857f2729badeb4ffa9fa63215a8
lksalbq <lks.albq@gmail.com>
2016-11-14 18:22:54 -0200

    Implementando niveis de acesso

--- config/deny-include.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!defined('MyConst')) {
-    if (!(isset($_SESSION['login']) == true) and ( isset($_SESSION['senha']) == true)) {
-        header('location: /prj_summer/index.php');
-    }
-}
